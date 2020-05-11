4d6cc258233983d25751f51d6ecd69a52d39ce6e
Mike James <michael.james6@gmail.com>
2020-05-11 13:50:03 +0100

    First commit

---
+++ ScriptDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System.Threading.Tasks;
+
+
+namespace ScriptDemo
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Write some C#");
+            var evaluate = true;
+
+            while(evaluate)
+            {
+                var input = Console.ReadLine();
+
+                if(input == "exit")
+                {
+                    evaluate = false;
+                    break;
+                }
+
+                if(input == "clear")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Write some C#");
+                    continue;
+                }
+
+                object result = await CSharpScript.EvaluateAsync(input);
+                Console.WriteLine(result.ToString());
+            }
+        }
+    }
+}
